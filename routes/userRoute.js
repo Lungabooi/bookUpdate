753b6d6745f8ef90433f7049474d490cfb5190ab
Lungabooi <Lungalucasbooi@gmail.com>
2022-08-25 13:44:02 +0200

    update verify

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -163,6 +163,32 @@ router.post("/register", (req, res) => {
       console.log(error);
     }
   });
+
+  
+  router.get("/users/verify", (req, res) => {
+      const token = req.header("x-auth-token");
+      jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+        if (error) {
+          res.status(401).json({
+            msg: "Unauthorized Access!",
+          });
+        } else {
+          res.status(200);
+          res.send(decodedToken);
+        }
+      });
+    });
+    router.get("/", middleware, (req, res) => {
+      try {
+        let sql = "SELECT * FROM users";
+        con.query(sql, (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
   
   
   
